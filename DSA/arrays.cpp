0d1aaa2c2d1568531342b0456c12f4d2e10c710b
Inder <inderchandel782@gmail.com>
2023-05-14 13:12:46 +0530

    changes done

--- DSA/arrays.cpp
+++ DSA/arrays.cpp
@@ -1,13 +1,5 @@
 #include <iostream>
-using namespace std;
 
-// intiiazling an array
 int main(){
-int arr[5]={1,2,3,4,6};
-
-for(int i =0;i<sizeof(arr)/sizeof(arr[0]);i++){
-    int num=arr[i];
-    cout<<num;
+    std::cout<<"hI MOM";
 }
-}
-
